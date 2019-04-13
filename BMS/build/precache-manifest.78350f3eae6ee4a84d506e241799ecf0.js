79dd93df40688714bc1c8da350b49c0cb6c72634
unknown <you@example.com>
2019-04-13 17:49:22 +0800

    app上传

---
+++ BMS/build/precache-manifest.78350f3eae6ee4a84d506e241799ecf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e76da352cbb9970cdf76",
+    "url": "/static/js/main.6927a6d1.chunk.js"
+  },
+  {
+    "revision": "0068a6d0fd92b289c599",
+    "url": "/static/js/2.6b18dd4f.chunk.js"
+  },
+  {
+    "revision": "e76da352cbb9970cdf76",
+    "url": "/static/css/main.bb7c6654.chunk.css"
+  },
+  {
+    "revision": "0068a6d0fd92b289c599",
+    "url": "/static/css/2.53c26424.chunk.css"
+  },
+  {
+    "revision": "ca1892169d7bd0d51d59b00e6331102a",
+    "url": "/index.html"
+  }
+];
